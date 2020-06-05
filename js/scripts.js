ef2c8aa0d8a6fc0b4c42b8e88378b97d20437345
Ruthie Newman <ruthienewman@protonmail.com>
2020-06-05 09:58:31 -0700

    add date type form to html, text type form to html, select option type form to index.html and radio button type form to index.html. Add variables for each form type in scripts.js so that values can be read .

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
+  const dob = $("#birthDate").val();
+  const hobby = $("#favHobby").val();
+  const season = $("#favSeason").val();
+  const color = $("#favColor").val();
 
+  const season = $("#favSeason").val();
+
+  const type = $("input:radio[name=jobType]:checked").val();
+
+  event.preventDefault();
 });

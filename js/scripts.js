4da30028122eaf133ca8a8f113ffcce806ac3581
Ruthie Newman <ruthienewman@protonmail.com>
2020-06-05 14:20:29 -0700

    add an else if condition and an else condition for job style values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,21 @@
-$(document).ready(function(){
-  $("#survey").submit(function(event){
-  const dob = $("#birthDate").val();
-  const hobby = $("#favHobby").val();
-  const color = $("#favColor").val();
-  const season = $("#favSeason").val();
+$(document).ready(function() {
+  $("#survey").submit(function(event) {
+  //const dob = $("#birthDate").val();
+  //const hobby = $("#favHobby").val();
+  //const color = $("#favColor").val();
+  //const season = $("#favSeason").val();
 
   const type = $("#jobStyle").val();
- 
 
   if (type === "creative") {
     $("#javaScript").show();
+    $("#python, #ruby").hide();
+  } else if (type === "analytical") {
+    $("#python").show(); 
+    $("#javaScript, #ruby").hide();
+  } else {
+    $("#ruby").show(); 
+    $("#javaScript, #python").hide();
   }
 
   event.preventDefault();
